1d3792c02d02669fc13575f31634f0bdbebf695c
amine <jmalmohamedamine1@gmail.com>
2025-07-26 14:19:23 +0100

    logs for debug in get api

--- back/src/controller/admin.controller/getEmployees.controller.ts
+++ back/src/controller/admin.controller/getEmployees.controller.ts
@@ -3,13 +3,13 @@ import { db } from "../../db";
 import { employees } from "../../db/schema";
 import { errorHelper } from "../../utils/errorHelper";
 export const getEmployeesController = async (c: Context) => {
-    try{
+    try {
+        console.log('Attempting to connect to database...');
         const result = await db.select().from(employees);
+        console.log('Query successful, rows:', result.length);
         return c.json(result);
-    }catch(error){
-
+    } catch (error) {
+        console.error('Database error:', error);
         return c.json(errorHelper.error(500, 'Internal Server Error'));
-
     }
-
 }
